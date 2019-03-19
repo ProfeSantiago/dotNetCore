11472f3aff76505d9f0dac6ac1eb590479f61ca5
Unibitec <Unibitec@users.noreply.github.com>
2019-03-18 23:09:36 -0600

    Repositorio Nuevo

---
+++ 1) .Net Core MVC/MiniCRUDMVC/MiniCRUDMVC/Controllers/PersonaController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MiniCRUDMVC.Models;
+
+namespace MiniCRUDMVC.Controllers
+{
+    public class PersonaController : Controller
+    {
+        ClasePersonaDB objetoPersona = new ClasePersonaDB();
+
+        //Ejemplo: https://localhost:44320/Persona/Cons_Personas
+        public IActionResult Cons_Personas()
+        {
+            try
+            {
+                return View(objetoPersona.Cons_Personas().ToList());
+            }
+            catch (Exception ex)
+            {
+                return View(new List<ModeloPersonas>());
+            }
+        }
+
+        //Ejemplo: https://localhost:44320/Persona/Ins_Personas
+        public IActionResult Ins_Personas(ModeloPersonas unaPesona)
+        {
+            try
+            {
+                objetoPersona.Ins_Personas(unaPesona);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ 1) .Net Core MVC/MiniCRUDMVC/MiniCRUDMVC/Models/ModeloPersonas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniCRUDMVC.Models
+{
+    public class ModeloPersonas
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+}
