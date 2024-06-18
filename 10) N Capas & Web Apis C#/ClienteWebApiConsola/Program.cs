61dd2a04f3c41bd50cdfaaa140c317ee4d35469e
Santiago Rodriguez Paniagua <santiago.rodriguez.paniagua@hotmail.com>
2024-06-17 19:20:48 -0600

    Primer Update Clase #5

---
+++ 10) N Capas & Web Apis C#/ClienteWebApiConsola/Program.cs
@@ -0,0 +1,31 @@
+﻿// Api que vamos a llamar:
+// https://jsonplaceholder.typicode.com/posts
+
+HttpClient client = new()
+{
+    BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
+};
+
+var response = await client.GetAsync("posts/1");
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine("Datos del primer Api:");
+Console.WriteLine(content);
+Console.WriteLine();
+Console.WriteLine("°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°-°");
+Console.WriteLine();
+
+//---------------------------------------------------------------
+// Api que vamos a llamar:
+// https://api.coindesk.com/v1/bpi/currentprice.json
+
+HttpClient client2 = new()
+{
+    BaseAddress = new Uri("https://api.coindesk.com/v1/bpi/")
+};
+
+var response2 = await client2.GetAsync("currentprice.json");
+var content2 = await response2.Content.ReadAsStringAsync();
+Console.WriteLine("Datos del segundo Api:");
+Console.WriteLine(content2);
+Console.WriteLine();
+//---------------------------------------------------------------
---
+++ 10) N Capas & Web Apis C#/WebApiClientConsoleParse/Program.cs
@@ -0,0 +1,34 @@
+﻿// Api que vamos a llamar:
+// https://jsonplaceholder.typicode.com/posts
+//
+namespace WebApiClientConsoleParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                //HTTP GET
+                var responseTask = client.GetAsync("posts");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<ApiDatos[]>();
+                    readTask.Wait();
+
+                    var TodosLosApiDatos = readTask.Result;
+
+                    foreach (var datoJSON in TodosLosApiDatos)
+                    {
+                        Console.WriteLine(datoJSON.title);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
